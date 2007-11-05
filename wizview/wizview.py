50fac2e4d58eeef55c363def79fdba044368d3de
Rob Taylor <rob.taylor@codethink.co.uk>
2007-11-05 22:40:14 +0000

    Add initial go at wizbit shares viewer

---
+++ wizview/wizview.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import sys
+try:
+    import pygtk
+    pygtk.require("2.0")
+except:
+    pass
+try:
+    import gobject
+    import gtk
+    import gtk.glade
+except:
+    sys.exit(1)
+
+
+from wizbit import ServiceBrowser
+
+class Wizview:
+
+    def __init__(self):
+        gladefile = "wizview.glade"
+        windowname = "wizview"
+        self.wTree=gtk.glade.XML (gladefile,windowname)
+        self.window = self.wTree.get_widget("wizview")
+        self.iconview = self.wTree.get_widget("shares")
+        self.sharelist = gtk.ListStore(gtk.gdk.Pixbuf, str)
+        self.iconview.set_model(self.sharelist)
+        self.iconview.set_pixbuf_column(0)
+        self.iconview.set_text_column(1)
+        self.sb = ServiceBrowser("_wizbit._tcp")
+        self.sb.connect ( "service-found", self._service_found)
+        self.sb.connect ( "service-removed", self._service_removed)
+        self.iconview.show()
+        self.window.show()
+        self.diricon = gtk.icon_theme_get_default().load_icon(gtk.STOCK_DIRECTORY, 48, gtk.ICON_LOOKUP_USE_BUILTIN)
+        self.items = {}
+
+    def _service_found(self, widget, name, type, interface, host, address, port):
+        print (name, type, interface, host, address, port);
+
+        self.items[name] = self.sharelist.append([self.diricon, host])
+
+    def _service_removed(self, widget, name):
+        self.sharelist.remove(self.items[name])
+        del self.items[name]
+
+app = Wizview()
+gtk.main()
