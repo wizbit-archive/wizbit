bf0bfba0f4f3345e48911f72083181ee96b62b39
Rob Taylor <rob.taylor@codethink.co.uk>
2007-11-16 18:16:48 +0000

    Convert Shares to an object. Allow creation of Shares objects that use a differetn storage file.

--- test/sharesunit.py
+++ test/sharesunit.py
@@ -1,39 +1,44 @@
 import unittest
 
 from os.path import exists, abspath
-from os import remove
+from os import remove, close
 from commands import getoutput
+from tempfile import mkstemp
 
 import wizbit
 from wizbit import Conf, Repo, Shares
 
-UUID1 = 'sdvseoviwjeocijweomc'
-UUID2 = 'segweawdq23e2aldasdk'
-UUID3 = 'schyawdo9iwdjoaismca'
-UUID4 = 'svsdIU3REFUSDsciuhdv'
+SHRID = 'e3a361cc-1710-44d3-8582-ac7ff13fd7c0'
+DIRID1 = '114a6a70-ff79-4cbb-8b91-6203eaef6afb'
+DIRID2 = '94b47f79-53ed-4958-8a01-60bb65feac6d'
+DIRID3 = 'f3e3e605-20ed-4200-b061-bfd75351e328'
+DIRID4 = 'de92b93d-a274-456a-b484-efb0427beae7'
 
 DIR1 = 'a/test/dir/one'
 DIR2 = 'a/test/dir/two'
 DIR3 = 'a/test/dir/three'
 DIR4 = 'a/test/dir/four'
 
-answer = [(UUID1, DIR1),
-		(UUID2, DIR2),
-		(UUID3, DIR3),
-		(UUID4, DIR4)]
+answer = [(DIRID1, SHRID, DIR1),
+	  (DIRID2, SHRID, DIR2),
+	  (DIRID3, SHRID, DIR3),
+	  (DIRID4, SHRID, DIR4)]
 
 class SharesCase(unittest.TestCase):
 	def testAdd(self):
-		Shares.addShare(UUID1, DIR1)
-		Shares.addShare(UUID2, DIR2)
-		Shares.addShare(UUID3, DIR3)
-		Shares.addShare(UUID4, DIR4)
-		result = Shares.getShares()
+		handle, path = mkstemp ("wizbit-test")
+		close(handle)
+		s = Shares(path)
+		s.addShare(DIRID1, SHRID, DIR1)
+		s.addShare(DIRID2, SHRID, DIR2)
+		s.addShare(DIRID3, SHRID, DIR3)
+		s.addShare(DIRID4, SHRID, DIR4)
+		result = s.getShares()
 		self.assertEquals(result, answer)
-		Shares.removeShare(UUID1)
-		Shares.removeShare(UUID2)
-		Shares.removeShare(UUID3)
-		Shares.removeShare(UUID4)
+		s.removeShare(DIRID1)
+		s.removeShare(DIRID2)
+		s.removeShare(DIRID3)
+		s.removeShare(DIRID4)
 
 if __name__ == '__main__':
 	unittest.main()
--- wizbit/shares.py
+++ wizbit/shares.py
@@ -4,64 +4,72 @@ from fcntl import flock, LOCK_EX, LOCK_SH
 
 DEFAULT_SHARES_PATH = os.environ["HOME"] + "/.wizdirs"
 
-__READ = LOCK_SH
-__WRITE = LOCK_EX
-def __lockFile(file, type):
-    # Wait forever to obtain the file lock
-    obtained = False
-    while (not obtained):
-        try:
-            flock(file, type)
-            obtained = True
-        except IOError:
-            pass
 
 
 class Shares:
     def __init__(self, shares_path = DEFAULT_SHARES_PATH):
-        self._shares_path = shares_path
+        self.shares_path = shares_path
+
+    def _lock(self, mode):
+        # Wait forever to obtain the file lock
+        mode = mode.strip('U')
+        if mode == 'r':
+            type = LOCK_SH
+        else:
+            type = LOCK_EX
+
+        obtained = False
+        while (not obtained):
+            try:
+                flock(self._share_file, type)
+                obtained = True
+            except IOError:
+                pass
+
 
     def _open (self, mode):
-        self._share_file = open(self._shares_path, "a")
-        _lockFile(self._share_file, mode)
+        self._share_file = open(self.shares_path, mode)
+        self._lock(mode)
 
-    def _close (self);
+    def _close (self):
         self._share_file.close()
 
-    def addShare(self, dirId, shareId, dir):
-        self._open(__WRITE)
+    def addShare(self, dirId, shareId, path):
+        self._open('a')
         try:
-            self._share_file.write("%s %s %s\n" % (dirId, shareId, dir))
+            self._share_file.write("%s %s %s\n" % (dirId, shareId, path))
         finally:
             self._close()
 
-    def removeShare(uuid):
-        self._open(__WRITE)
+    def removeShare(self, dirId):
+        self._open('r+')
         try:
             input = self._share_file.readlines()
             self._share_file.seek(0)
             for line in input:
-                (lineid, shrId, dir) = line.split()[0:3]
-                if lineid != uuid:
+                (lineDirId, shrId, path) = line.split()[0:3]
+                if lineDirId != dirId:
                     self._share_file.write(line)
-            shareFile.truncate()
+            self._share_file.truncate()
         finally:
             self._close()
 
-    def getShares():
-        self._open(__READ)
+    def getShares(self):
+        self._open('r')
         try:
             shares = []
             for line in self._share_file:
                 if line:
-                    (id, shrId, dir) = line.split()[0:3]
-                    shares.append((id, shrId, dir))
+                    (dirId, shrId, path) = line.split()[0:3]
+                    shares.append((dirId, shrId, path))
         finally:
             self._close()
         return shares
 
+
+__default_shares = None
 def defaultShares():
     global __default_shares
-    if not __default_shares:
+    if __default_shares == None:
         __default_shares = Shares();
     return __default_shares
