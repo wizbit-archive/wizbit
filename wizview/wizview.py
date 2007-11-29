dc8c08ec4d36a898a93a916d6ae1d0ff19463d41
Rob Taylor <rob.taylor@codethink.co.uk>
2007-11-29 10:46:30 +0000

    Make mount dialog show both available and mounted shares. Set mount/unmount buttons insensitve appropriately for the currently selected share.

--- wizview/wizview.py
+++ wizview/wizview.py
@@ -17,6 +17,7 @@ import xmlrpclib
 from pprint import pprint
 from wizbit import SharesDatabase, defaultShares
 
+
 class ShareSelectDialog:
     def __init__(self):
         gladefile = "wizview.glade"
@@ -28,43 +29,53 @@ class ShareSelectDialog:
         self.iconview.set_model(self.sharelist)
         self.iconview.set_pixbuf_column(0)
         self.iconview.set_text_column(1)
-        self.sdb = SharesDatabase()
-        self.sdb.connect ( "updated", self._updated)
         self.iconview.show()
         self.window.show()
         self.diricon = gtk.icon_theme_get_default().load_icon(gtk.STOCK_DIRECTORY, 48, gtk.ICON_LOOKUP_USE_BUILTIN)
         self.items = {}
 
-    def _updated(self, sdb):
-        pprint(sdb.shares, width=160)
-        self.sharelist.clear()
-        for s in sdb.shares.keys():
-            self.sharelist.append([self.diricon, s])
-
 
 
 class MountsDialog:
+
+    MOUNT_CANCEL = 1
+    MOUNT_SELECT = 1
 
     def __init__(self):
         gladefile = "wizview.glade"
         windowname = "mounts_dialog"
         self.wTree=gtk.glade.XML (gladefile,windowname)
         self.window = self.wTree.get_widget(windowname)
+
         self.mountsview = self.wTree.get_widget("mounts_table")
-        self.mounts = gtk.ListStore(str, str)
+        #liststore has shareid, share name, mountpath, mounted
+        self.mounts = gtk.ListStore(str, str, str, 'gboolean')
         self.mountsview.set_model(self.mounts)
+
         cellpb = gtk.CellRendererPixbuf()
-        column = gtk.TreeViewColumn ("Mounted Share", cellpb)
+        column = gtk.TreeViewColumn ("Share", cellpb)
         cell = gtk.CellRendererText()
         column.pack_start(cell, False)
         column.set_cell_data_func(cellpb, self._get_dir_icon)
-        column.set_attributes(cell, text=0)
-        self.mountsview.append_column(column)
-        cell = gtk.CellRendererText()
-        column = gtk.TreeViewColumn ("Path", cell)
         column.set_attributes(cell, text=1)
         self.mountsview.append_column(column)
+        cell = gtk.CellRendererText()
+        column = gtk.TreeViewColumn ("Mount Path", cell)
+        column.set_attributes(cell, text=2)
+
+        self.mountsview.connect("cursor_changed", self._cursor_moved);
+
+        self.sdb = SharesDatabase()
+        self.sdb.connect ( "updated", self._sdb_updated)
+
+        self.mountsview.append_column(column)
         self.mountsview.show()
+
+        self.wTree.get_widget("mount_share").connect("clicked", self._mount_clicked)
+        self.wTree.get_widget("umount_share").connect("clicked", self._umount_clicked)
+        self.wTree.get_widget("mount_share").set_sensitive(False)
+        self.wTree.get_widget("umount_share").set_sensitive(False)
+
         self.window.show()
         self.diricon = gtk.icon_theme_get_default().load_icon(gtk.STOCK_DIRECTORY, 16, gtk.ICON_LOOKUP_USE_BUILTIN)
         self.items = {}
@@ -72,10 +83,45 @@ class MountsDialog:
 
     def _fill_local_shares(self):
         for dirId,shrId,path in defaultShares().getShares():
-            self.mounts.append([dirId,path])
+            self.items[shrId] = (shrId, path, True)
+        self._populate_list()
 
     def _get_dir_icon(self, column, cell, model, iter):
         cell.set_property('pixbuf', self.diricon)
+
+    def _sdb_updated(self, sdb):
+        pprint(sdb.shares, width=160)
+        for s in sdb.shares.keys():
+            self.items[s] = (s, None, False)
+        self._populate_list()
+
+    def _populate_list(self):
+        self.mounts.clear()
+        print self.items
+        for (shrId, (name, path, mount)) in self.items.items():
+            print shrId, name, path, mount
+            self.mounts.append([shrId, name, path, mount])
+
+    def _cursor_moved(self, treeview):
+        path, column = treeview.get_cursor()
+        iter = self.mounts.get_iter(path)
+        (mounted,) = self.mounts.get(iter,3)
+        print path, mounted
+        self.wTree.get_widget("mount_share").set_sensitive(not mounted)
+        self.wTree.get_widget("umount_share").set_sensitive(mounted)
+
+    def _mount_clicked(self, button):
+        path, column = self.mountsview.get_cursor()
+        iter = self.mounts.get_iter(path)
+        (name,) = self.mounts.get(iter,1)
+        pathchooser = gtk.FileChooserDialog(title="Please choose a folder in which to mount share '%s'" % name, parent = self.window, action= gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER, buttons=(("Cancel", self.MOUNT_CANCEL),("Select", self.MOUNT_SELECT)))
+        response = pathchooser.run()
+        if response == self.MOUNT_SELECT:
+            pass
+        pathchooser.hide()
+        pathchooser.destroy()
+    def _umount_clicked(self, button):
+        pass
 
 app = MountsDialog()
 gtk.main()
