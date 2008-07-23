c9367e79bb48abb981fac19d6d0a4934db634a5c
John Carr <john.carr@unrouted.co.uk>
2008-07-23 15:11:24 +0100

    Make file.h suck less

--- wizbit/file.h
+++ wizbit/file.h
@@ -1,20 +1,24 @@
 #ifndef WIZBIT_FILE_H
 #define WIZBIT_FILE_H
 
-typedef wiz_vref char[20];
+typedef unsigned char wiz_vref[20];
+
+enum wiz_file_mode {
+	WIZ_FILE_MODE_LAST
+};
 
 struct wiz_file;
 
 struct wiz_file *wiz_file_new();
 
-struct wiz_file *wiz_file_open(wiz_vref ref, int flags, mode_t mode);
+struct wiz_file *wiz_file_open(wiz_vref ref, int flags, enum wiz_file_mode mode);
 
-void wiz_file_add_parent(wiz_file *file, wiz_vref ref);
+void wiz_file_add_parent(struct wiz_file *file, wiz_vref ref);
 
-const wiz_vref *wiz_file_snapshot(wiz_file *file);
+const wiz_vref *wiz_file_snapshot(struct wiz_file *file);
 
-void wiz_file_close();
+void wiz_file_close(struct wiz_file *file);
 
-int wiz_file_get_fd(wiz_file *file);
+int wiz_file_get_fd(struct wiz_file *file);
 
-#endif WIZBIT_FILE_H
+#endif /* WIZBIT_FILE_H */
