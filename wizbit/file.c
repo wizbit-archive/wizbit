deb7b6e7853de4f89ab3821016e1463705ebb392
John Carr <john.carr@unrouted.co.uk>
2008-07-28 15:01:45 +0100

    Autotool fixing

--- wizbit/file.c
+++ wizbit/file.c
@@ -1,6 +1,10 @@
 #include <glib.h>
 #include <glib/gstdio.h>
 
+#include <libgitcore/sha1.h>
+#include <libgitcore/object-loader.h>
+#include <libgitcore/loose-object-loader.h>
+
 #include "file.h"
 #include "vref.h"
 
@@ -17,7 +21,8 @@ struct wiz_file *wiz_file_open(wiz_vref ref, int flags, enum wiz_file_mode mode)
 	if (wiz_vref_compare(ref, WIZ_FILE_NEW)) {
 		file->fd = g_mkstemp("/tmp/WIZBIT_XXXXXX");
 	} else {
-		
+		struct git_object_loader *loader;
+		loader = git_object_loader_new("/tmp/wizbit");
 	}
 
 	return file;
