193020c2d55c3c48854caaf4b236b1145028ecf2
John Carr <john.carr@unrouted.co.uk>
2008-07-29 10:34:45 +0100

    Need to do something like this to generate a commit

--- wizbit/file.c
+++ wizbit/file.c
@@ -3,10 +3,12 @@
 
 #include <libgitcore/sha1.h>
 #include <libgitcore/object-loader.h>
-#include <libgitcore/loose-object-loader.h>
 #include <libgitcore/commit.h>
 #include <libgitcore/tree.h>
+#include <libgitcore/blob.h>
 #include <libgitcore/tree-iterator.h>
+
+#include <libgitcore/loose-object-writer.h>
 
 #include "file.h"
 #include "vref.h"
@@ -64,6 +66,35 @@ void wiz_file_add_parent(struct wiz_file *file, wiz_vref ref)
 
 void wiz_file_snapshot(struct wiz_file *file, wiz_vref ref)
 {
+        struct git_object_loader *loader;
+        struct git_loose_object_writer *writer;
+        struct git_blob_writer *blob_writer;
+        git_sha1 blob;
+        struct git_tree_writer *tree_writer;
+        git_sha1 tree;
+        struct git_commit_writer *commit_writer;
+        git_sha1 commit;
+        struct git_error *error;
+
+        loader = git_object_loader_new("/tmp/wizbit");
+        writer = git_loose_object_writer_new(loader,"/tmp/wizbit");
+
+        blob_writer = git_blob_writer_new();
+        git_blob_writer_set_contents(blob_writer, g_mapped_file_get_contents(file->gfile),
+                                     g_mapped_file_get_length(file->gfile));
+        git_blob_writer_write(blob_writer, writer, &error);
+
+        tree_writer = git_tree_writer_new();
+        git_tree_writer_add_sha1(tree_writer, 0, "path", blob);
+        git_tree_writer_write(tree_writer, writer, &error);
+
+        commit_writer = git_commit_writer_new();
+        git_commit_writer_set_tree_sha1(commit_writer, tree);
+        /* git_commit_writer_add_parent_sha1(commit_writer, parent); */
+        git_commit_writer_set_author(commit_writer, "John Carr <john.carr@unrouted.co.uk>", 0);
+        git_commit_writer_set_committer(commit_writer, "John Carr <john.carr@unrouted.co.uk>", 0);
+        git_commit_writer_set_message(commit_writer, "Loreum Ipsum");
+        git_commit_writer_write(commit_writer, writer, &error);
 }
 
 void wiz_file_close(struct wiz_file *file)
