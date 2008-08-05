2e10c3f56425ba16ffc079ce19f2b4c7b37c3b3c
John Carr <john.carr@unrouted.co.uk>
2008-08-05 16:55:40 +0100

    We should have some commit chains in our testing

--- test/test-file.c
+++ test/test-file.c
@@ -12,10 +12,25 @@ int main()
 		wiz_vref vref;
 		FILE *fp;
 
+		/*
+		   Open up a new versioned file and create a couple
+		   of revisions
+		*/
+
 		file = wiz_file_open(WIZ_FILE_NEW, 0, 0);
 		fp = wiz_file_get_handle(file);
+
 		fprintf(fp, "I BELIEVE");
 		wiz_file_snapshot(file, vref);
+
+		fprintf(fp, "\nNO RLY");
+		wiz_file_add_parent(file, vref);
+		wiz_file_snapshot(file, vref);
+
+		fprintf(fp, "\nI CAN HAS BELIEVE!?");
+		wiz_file_add_parent(file, vref);
+		wiz_file_snapshot(file, vref);
+
 		wiz_file_close(file);
 	}
 
